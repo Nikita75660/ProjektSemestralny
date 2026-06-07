dcff3aee32ab323e3f2707ec2aa9723635a12e8a
Nikita75660 <nkorcha1@stu.vistula.edu.pl>
2026-06-07 17:14:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://xohpancxryzudbofmdtv.supabase.co";
+const SUPABASE_URL = "https://xohpancxryzudbofmdtv.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvaHBhbmN4cnl6dWRib2ZtZHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mjg0OTQsImV4cCI6MjA5NjQwNDQ5NH0.v9D9ZoqStAokNXbXtLsneOTca-CZF5kDgQQZ-eHjmMM";
 
 const supabase = window.supabase.createClient(
